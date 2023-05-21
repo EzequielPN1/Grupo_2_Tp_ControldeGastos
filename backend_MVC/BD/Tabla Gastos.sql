3d13b947363d864d1ff13f44ff5af559c405c26a
EzequielPN1 <ezequiel.sitepro@gmail.com>
2023-05-21 20:17:23 -0300

    listo

--- backend_MVC/BD/Tabla Gastos.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE Gastos (
-  email TEXT PRIMARY KEY,
-  gasto REAL,
-  titulo TEXT,
-  fecha DATE,
-  categoria_id INTEGER,
-  descripcion TEXT,
-  FOREIGN KEY (categoria_id) REFERENCES Categoria(id)
-);
