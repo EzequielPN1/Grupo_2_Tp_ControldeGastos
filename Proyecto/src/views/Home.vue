bfa47971c758abcdc92cf67039a00912500f2b67
EzequielPN1 <ezequiel.sitepro@gmail.com>
2023-05-27 23:07:54 -0300

    graficoHome

--- Proyecto/src/views/Home.vue
+++ Proyecto/src/views/Home.vue
@@ -1,22 +1,71 @@
-<script>
-import Barra from "../views/Barra.vue"
-export default {
+<template>
+  <div>
+    <Barra></Barra>
 
-    components: {
-    Barra,
+    <div>
+      <canvas id="myChart"></canvas>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Barra from "../views/Barra.vue";
+import { useUserStore } from "../stores/user";
+import { useGastosStore } from "../stores/gastos.js";
+import Chart from 'chart.js/auto';
+
+export default {
+  components: {
+    Barra
   },
+  async mounted() {
+    await this.actualizarGastos();
+    this.mostrarGrafico();
+
+  },
+  data() {
+    return {
+      gastos: []
+    }
+  },
+  methods: {
+    async actualizarGastos() {
+      const userStore = useUserStore();
+      const { usuario } = userStore;
+
+      const gastosStore = await useGastosStore();
+      await gastosStore.obtenerGastos(usuario.email);
+      this.gastos = gastosStore.gastos;
+    },
+    mostrarGrafico() {
+  const ctx = document.getElementById('myChart');
+
+  // Obtener las fechas y montos de this.gastos
+  const labels = this.gastos.map(gasto => gasto.fecha);
+  const data = this.gastos.map(gasto => gasto.monto);
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: labels,
+      datasets: [{
+        label: '# of Votes',
+        data: data,
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
 }
 
 
+  }
+};
 </script>
-
-
-
-
-<template>
-
-<Barra></Barra>
-
-
-
-</template>
