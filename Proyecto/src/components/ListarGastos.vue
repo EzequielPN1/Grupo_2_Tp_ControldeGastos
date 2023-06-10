8d2425106798975d89706706cf1cfc153f9b394d
EzequielPN1 <ezequiel.sitepro@gmail.com>
2023-06-10 10:36:17 -0300

    filtroGastosPorMenorMayor-fecha

--- Proyecto/src/components/ListarGastos.vue
+++ Proyecto/src/components/ListarGastos.vue
@@ -1,144 +1,3 @@
-<script>
-import { useUserStore } from "../stores/user";
-import { useGastosStore } from "../stores/gastos.js";
-import { useCategoriaStore } from "../stores/categorias.js";
-import { gastosService } from "../Services/gastosService.js";
-import Barra from "../components/NavBar.vue";
-import { tokenService } from "../Services/tokenService.js"
-
-export default {
-  
-  created() {
-    tokenService.validarUsuarioRecarga(this, this.loadData)
-  },
-
-  setup() {
-    const { usuario } = useUserStore();
-    const storeCategoria = useCategoriaStore();
-    const gastosStore = useGastosStore();
-    return {
-      usuario, storeCategoria, gastosStore
-    };
-  },
-
-  data() {
-    return {
-      gastos: [],
-      categorias: [],
-      categoriaSeleccionada: "",
-    
-    };
-  },
-  computed: {
-    gastosFiltrados() {
-      if (this.categoriaSeleccionada === "") {
-        return this.gastos;
-      } else {
-        return this.gastos.filter(
-          (gasto) => gasto.idCategoria === this.categoriaSeleccionada
-        );
-      }
-    },
-  },
-  methods: {
-
-    async loadData() {
-      await this.actualizarGastos();
-      await this.obtenerCategorias();
-    },
-    async obtenerCategorias() {
-      await this.storeCategoria.obtenerCategorias(this.usuario.email);
-      this.categorias = this.storeCategoria.categorias;
-    },
-
-    async actualizarGastos() {
-      await this.gastosStore.obtenerGastos(this.usuario.email);
-      this.gastos = this.gastosStore.gastos;
-
-      this.gastos.forEach((gasto) => {
-        const categoria = this.categorias.find(
-          (c) => c.id === gasto.idCategoria
-        );
-        gasto.nombreCategoria = categoria ? categoria.nombre : "";
-      });
-    },
-
-    editarGasto(gasto) {
-      gasto.editando = true;
-    },
-
-
-    async guardarGasto(gasto) {
-    
-    try {
-      await tokenService.validarToken(this.usuario, this.$router)
-
-      const categoria = this.categorias.find(categoria => categoria.id === gasto.idCategoria);
-      const presupuesto = categoria.presupuesto
-
-      const mes = gasto.fecha.slice(5, 7);
-
-      const gastosMismaCategoria = this.gastos.filter(gasto => gasto.idCategoria === categoria.id && gasto.fecha.slice(5, 7) === mes);
-
-      let sumaGastos = 0;
-      gastosMismaCategoria.forEach(gasto => {
-        sumaGastos += gasto.monto;
-      });
-     
-
-      console.log("la suma con el gasto nuevo incluido: " + sumaGastos);
-      console.log("el presupuesto de la categoria base: " +presupuesto);
-      
-
-      await gastosService.editarGasto(gasto);
-      console.log("Gasto editado correctamente.");
-      await this.actualizarGastos();
-
-      if (sumaGastos>presupuesto) {
-        alert("El gasto  supera el presupuesto");
-      } 
-
-    } catch (error) {
-      alert("Error al editar el gasto." + error.response.data);
-    }
-    gasto.editando = false;
-
-  },
-
-
-
-
-
-
-
-
-
-    async eliminarGasto(gasto) {
-      try {
-        await tokenService.validarToken(this.usuario, this.$router)
-        await gastosService.eliminarGasto(gasto);
-        await this.actualizarGastos();
-        alert("Gasto eliminado correctamente.");
-      } catch (error) {
-        console.log(error);
-        alert("Error al eliminar el gasto." + error.response.data);
-      }
-    },
-
-    getCategoriaNombre(idCategoria) {
-      const categoria = this.categorias.find((c) => c.id === idCategoria);
-      return categoria ? categoria.nombre : "";
-    },
-
-  },
-  components: {
-    Barra,
-  },
-
-
-};
-</script>
-
 <template>
   <Barra></Barra>
   <div>
@@ -146,6 +5,74 @@ export default {
     <select id="categoria" v-model="categoriaSeleccionada">
       <option value="">Todos</option>
       <option v-for="categoria in categorias" :value="categoria.id">{{ categoria.nombre }}</option>
+    </select>
+
+    <label for="anio">Filtrar por año:</label>
+    <select id="anio" v-model="filtroAnio">
+      <option value="">Todos</option>
+      <option value="2020">2020</option>
+      <option value="2021">2021</option>
+      <option value="2022">2022</option>
+      <option value="2023">2023</option>
+    </select>
+
+    <label for="mes">Filtrar por mes:</label>
+    <select id="mes" v-model="filtroMes">
+      <option value="">Todos</option>
+      <option value="01">Enero</option>
+      <option value="02">Febrero</option>
+      <option value="03">Marzo</option>
+      <option value="04">Abril</option>
+      <option value="05">Mayo</option>
+      <option value="06">Junio</option>
+      <option value="07">Julio</option>
+      <option value="08">Agosto</option>
+      <option value="09">Septiembre</option>
+      <option value="10">Octubre</option>
+      <option value="11">Noviembre</option>
+      <option value="12">Diciembre</option>
+    </select>
+
+    <label for="dia">Filtrar por día:</label>
+    <select id="dia" v-model="filtroDia">
+      <option value="">Todos</option>
+      <option value="01">1</option>
+      <option value="02">2</option>
+      <option value="03">3</option>
+      <option value="04">4</option>
+      <option value="05">5</option>
+      <option value="06">6</option>
+      <option value="07">7</option>
+      <option value="08">8</option>
+      <option value="09">9</option>
+      <option value="10">10</option>
+      <option value="11">11</option>
+      <option value="12">12</option>
+      <option value="13">13</option>
+      <option value="14">14</option>
+      <option value="15">15</option>
+      <option value="16">16</option>
+      <option value="17">17</option>
+      <option value="18">18</option>
+      <option value="19">19</option>
+      <option value="20">20</option>
+      <option value="21">21</option>
+      <option value="22">22</option>
+      <option value="23">23</option>
+      <option value="24">24</option>
+      <option value="25">25</option>
+      <option value="26">26</option>
+      <option value="27">27</option>
+      <option value="28">28</option>
+      <option value="29">29</option>
+      <option value="30">30</option>
+      <option value="31">31</option>
+    </select>
+
+    <label for="orden">Ordenar:</label>
+    <select id="orden" v-model="orden">
+      <option value="asc">Menor a mayor</option>
+      <option value="desc">Mayor a menor</option>
     </select>
 
     <table class="table">
@@ -217,6 +144,136 @@ export default {
     </table>
   </div>
 </template>
+
+
+<script>
+import { useUserStore } from "../stores/user";
+import { useGastosStore } from "../stores/gastos.js";
+import { useCategoriaStore } from "../stores/categorias.js";
+import { gastosService } from "../Services/gastosService.js";
+import Barra from "../components/NavBar.vue";
+import { tokenService } from "../Services/tokenService.js"
+
+export default {
+  created() {
+    tokenService.validarUsuarioRecarga(this, this.loadData);
+  },
+
+  setup() {
+    const { usuario } = useUserStore();
+    const storeCategoria = useCategoriaStore();
+    const gastosStore = useGastosStore();
+    return {
+      usuario,
+      storeCategoria,
+      gastosStore,
+    };
+  },
+
+  data() {
+    return {
+      gastos: [],
+      categorias: [],
+      categoriaSeleccionada: "",
+      filtroAnio: "",
+      filtroMes: "",
+      filtroDia: "",
+      orden: "desc",
+    };
+  },
+
+  computed: {
+    gastosFiltrados() {
+      let gastos = this.gastos;
+
+      if (this.categoriaSeleccionada !== "") {
+        gastos = gastos.filter(
+          (gasto) => gasto.idCategoria === this.categoriaSeleccionada
+        );
+      }
+
+      if (this.filtroAnio !== "") {
+        gastos = gastos.filter(
+          (gasto) => gasto.fecha.slice(0, 4) === this.filtroAnio
+        );
+      }
+
+      if (this.filtroMes !== "") {
+        gastos = gastos.filter(
+          (gasto) => gasto.fecha.slice(5, 7) === this.filtroMes
+        );
+      }
+
+      if (this.filtroDia !== "") {
+        gastos = gastos.filter(
+          (gasto) => gasto.fecha.slice(8, 10) === this.filtroDia
+        );
+      }
+
+      if (this.orden === "asc") {
+        gastos = gastos.sort((a, b) => a.fechaNumerica - b.fechaNumerica);
+      }
+      if (this.orden === "desc") {
+        gastos = gastos.sort((a, b) => b.fechaNumerica - a.fechaNumerica);
+      }
+
+      return gastos;
+    },
+  },
+
+  methods: {
+    async loadData() {
+      await this.actualizarGastos();
+      await this.obtenerCategorias();
+    },
+
+    async obtenerCategorias() {
+      await this.storeCategoria.obtenerCategorias(this.usuario.email);
+      this.categorias = this.storeCategoria.categorias;
+    },
+
+    async actualizarGastos() {
+      await this.gastosStore.obtenerGastos(this.usuario.email);
+      this.gastos = this.gastosStore.gastos;
+
+      this.gastos.forEach((gasto) => {
+        const categoria = this.categorias.find((c) => c.id === gasto.idCategoria);
+        gasto.fechaNumerica = new Date(gasto.fecha).getTime();
+        gasto.editando = false;
+        gasto.categoria = categoria;
+      });
+    },
+
+    getCategoriaNombre(idCategoria) {
+      const categoria = this.categorias.find((c) => c.id === idCategoria);
+      return categoria ? categoria.nombre : "";
+    },
+
+    async editarGasto(gasto) {
+      gasto.editando = true;
+    },
+
+    async guardarGasto(gasto) {
+      await gastosService.actualizarGasto(gasto);
+      gasto.editando = false;
+    },
+
+    async eliminarGasto(gasto) {
+      if (confirm("¿Estás seguro de eliminar el gasto?")) {
+        await gastosService.eliminarGasto(gasto.id);
+        await this.actualizarGastos();
+      }
+    },
+  },
+
+  components: {
+    Barra,
+  },
+};
+</script>
+
+
+
 
 <style>
 .table {
